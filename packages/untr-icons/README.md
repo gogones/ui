074a051a87a662f85fc77f7b49c77cf9fc845ff8
Annas Alghoffar <aag.annas@gmail.com>
2024-11-20 10:12:09 +0700

    feat: new icons

---
+++ packages/untr-icons/README.md
@@ -0,0 +1,7 @@
+# icons
+
+Icon collection for untr-ui
+
+## License
+
+Licensed under the [MIT license](https://github.com/shadcn/ui/blob/main/LICENSE.md).
---
+++ packages/untr-icons/src/index.ts
@@ -0,0 +1,9 @@
+export * from "./components/InformationCircleIcon"
+export * from "./components/WarningErrorIcon"
+export * from "./components/CheckCircleIcon"
+export * from "./components/RemoveCircleIcon"
+
+export { default as InformationCircleIcon } from "./components/InformationCircleIcon"
+export { default as WarningErrorIcon } from "./components/WarningErrorIcon"
+export { default as CheckCircleIcon } from "./components/CheckCircleIcon"
+export { default as RemoveCircleIcon } from "./components/RemoveCircleIcon"
